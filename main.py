86b62a0d7f601a5a6fd3924ba1e0e77b78b6b61d
serhiivkovalenko <adbc@ukr.net>
2023-04-30 00:06:52 +0300

    task5 is added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from task1 import task1
 from task2 import task2
 from task3 import task3
 from task4 import task4
+from task5 import task5
 
 
 def main():
@@ -12,13 +13,15 @@ def main():
                                  .appName("IMDB")
                                  .config(conf=SparkConf())
                                  .getOrCreate())
-    print("Task1")
+    # print("Task1")
     # task1(spark_session)
     # task2(spark_session)
     # print("Task3")
     # task3(spark_session)
-    print("Task4")
-    task4(spark_session)
+    # print("Task4")
+    # task4(spark_session)
+    print("Task5")
+    task5(spark_session)
 
 
 if __name__ == "__main__":
