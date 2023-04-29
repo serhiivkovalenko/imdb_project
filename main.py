bb116798adcc5c28ff93b24a8baeac54bd627170
serhiivkovalenko <adbc@ukr.net>
2023-04-29 12:46:12 +0300

    task3 is added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from task1 import task1
 from task2 import task2
+from task3 import task3
 
 
 def main():
@@ -11,7 +12,9 @@ def main():
                                  .config(conf=SparkConf())
                                  .getOrCreate())
     # task1(spark_session)
-    task2(spark_session)
+    # task2(spark_session)
+    print("Task3")
+    task3(spark_session)
 
 
 if __name__ == "__main__":
